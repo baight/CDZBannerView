97b5721ac245a34b14b7781f19d4d6e34ef03d0b
baight <cz-cczz@163.com>
2015-07-23 15:56:10 +0800

    添加注释

--- CDZBannerView.h
+++ CDZBannerView.h
@@ -2,7 +2,7 @@
 //  CDZBannerView.h
 //
 //
-//  Created by cui baight on 13-5-29.
+//  Created by baight on 13-5-29.
 //  Copyright (c) 2013年 baight. All rights reserved.
 //
 
@@ -12,7 +12,7 @@
 /*
  
  BannerView 是一个 可以实现若干张图片图片循环滚动的控件
- BannerView 中某张图片被点击时，会向 委托 发送 bannerViewDidClicked: 消息，参数为图片索引
+ BannerView 中某张图片被点击时，如果canClickImage属性为YES，则会向 委托 发送 bannerViewDidClicked: 消息，参数为图片索引
  
  属性 imageArray 是一个数组，里边储存着要循环显示的图片信息
  如果 imageArray 成员若是 NSURL 类，则展示网络图片，网络图片未下载下来时，显示 属性placeholderImage图片
@@ -36,15 +36,15 @@
 
 @property (nonatomic, strong, readonly) UIPageControl* pageControl;
 
-@property (nonatomic, weak) IBOutlet id delegate;
+@property (nonatomic, assign) IBOutlet id delegate;
 
 @property (nonatomic, retain) NSArray* imageArray;            // 要显示的图片信息数组
 @property (nonatomic, retain) UIImage* placeholderImage;      // 网络图片未下载下来时，显示该本地图片
 @property (nonatomic, assign) NSTimeInterval timeInterval;    // 图片自动滚动间隔，默认为0，不自动滚动
 @property (nonatomic, assign) NSInteger currentPage;          // 当前显示第几页,从 0 开始计算
 
-@property (nonatomic, assign) BOOL canClickImage;
-@property (nonatomic, assign) BOOL canCycleScroll;
+@property (nonatomic, assign) BOOL canClickImage;    // 默认为NO
+@property (nonatomic, assign) BOOL canCycleScroll;   // 是否可以循环滚动，默认YES
 
 -(void)setImageArray:(NSArray *)imageArray initPage:(NSInteger)initPage;
 
